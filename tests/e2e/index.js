c493c130007d25c2b58634a7f31746f3f997103f
Simon Babay <simon@babay.fr>
2018-07-01 14:59:14 +0200

    Dev project with linked packages
    For faster init & tests

--- tests/e2e/index.js
+++ tests/e2e/index.js
@@ -1,35 +1,26 @@
 const argv = require('yargs').argv;
 const fs = require('fs-extra');
 const { join } = require('path');
-const createProject = require('./scripts/createProject');
+const execa = require('execa');
 const copyPackages = require('./scripts/copyPackages');
 const invoke = require('./scripts/invoke');
 const run = require('./scripts/run');
 
 (async () => {
-  const projectName = 'project';
-  const projectPath = join(__dirname, `../${projectName}`);
+  const projectName = 'dev';
+  const projectPath = join(__dirname, `../../packages/${projectName}`);
 
   // vue create
   if (!argv.quick) {
-    await createProject(
-      projectPath,
-      {
-        plugins: {
-          '@vue/cli-plugin-babel': {},
-        },
-      },
-      process.cwd(),
-    );
-
-    // vue invoke
-    await invoke(projectPath);
+    await execa(join(__dirname, '../../dev.sh'), {
+      stdio: 'inherit',
+    });
   }
 
   // ---/---
 
   // Copy mocks
-  await fs.copy('tests/e2e/mocks', 'tests/project');
+  await fs.copy('tests/e2e/mocks', projectPath);
 
   // Use current sources to tests project
   await copyPackages(projectPath);
