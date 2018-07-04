3501e3d95d2e375276e4490c479a754c43703299
Simon Babay <simon@babay.fr>
2018-07-04 11:08:16 +0200

    (cli-plugins) TypeScript support

--- packages/@vueneue/vue-cli-plugin-ssr/generator/index.js
+++ packages/@vueneue/vue-cli-plugin-ssr/generator/index.js
@@ -44,6 +44,8 @@ module.exports = (api, options) => {
 
   // TypeScript support
   if (api.invoking && api.hasPlugin('typescript')) {
+    api.render('./typescript');
+
     /* eslint-disable-next-line node/no-extraneous-require */
     const convertFiles = require('@vue/cli-plugin-typescript/generator/convert');
     convertFiles(api);
