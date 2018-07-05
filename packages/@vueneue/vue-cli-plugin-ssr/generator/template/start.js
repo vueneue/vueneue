b6d50c6a565a6074dede793a6b07c598c7988eda
Simon Babay <simon@babay.fr>
2018-07-05 17:32:17 +0200

    (plugin) Cleaning template

--- packages/@vueneue/vue-cli-plugin-ssr/generator/template/start.js
+++ packages/@vueneue/vue-cli-plugin-ssr/generator/template/start.js
@@ -7,6 +7,10 @@ const { existsSync } = require('fs');
 const path = require('path');
 const serverStart = require('@vueneue/ssr-server');
 
+// Force production mode
+process.env.NODE_ENV = 'production';
+
+// Read Vue config if exists
 let ssr;
 if (existsSync('./vue.config.js')) {
   const { pluginOptions } = require('./vue.config.js');
