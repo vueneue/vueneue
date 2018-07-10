2e3be7fe3c997ec4d0a95ca88e598b011391cb97
Simon Babay <simon@babay.fr>
2018-07-10 15:05:54 +0200

    (ts-plugin) Add definitions files to core

---
+++ packages/@vueneue/ssr-core/index.d.ts
@@ -0,0 +1,42 @@
+/// <reference path="./global.d.ts"/>
+
+import Vue from 'vue';
+import { Store } from 'vuex';
+import VueRouter, { Route } from 'vue-router';
+import { MetaInfo } from 'vue-meta';
+
+/**
+ * Vue meta
+ */
+declare module 'vue/types/vue' {
+  interface Vue {
+    head?: MetaInfo | (() => MetaInfo);
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    head?: MetaInfo | (() => MetaInfo);
+  }
+}
+
+/**
+ * VueNeue
+ */
+declare module 'vue/types/vue' {
+  interface Vue {
+    $error: (error: any, statusCode?: number) => void;
+    $redirect: (location: any, statusCode?: number) => void;
+    $context: {
+      app: Vue;
+      router: VueRouter;
+      store: Store<any>;
+      url: string;
+      route: Route;
+      query: any;
+      params: any;
+      reditect: (location: any, statusCode?: number) => void;
+      error: (error: any, statusCode?: number) => void;
+    };
+  }
+}
