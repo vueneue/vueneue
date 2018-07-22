07a59282c074739f6baecd77eab9c8b05f9c5329
Simon Babay <simon@babay.fr>
2018-07-23 01:46:01 +0200

    (tests) Update changes

--- tests/specs/ci/spa-routes.spec.js
+++ tests/specs/ci/spa-routes.spec.js
@@ -1,4 +1,4 @@
-const { readSSRData, checkText, baseURL, isMounted } = require('../utils');
+const { isSPA, checkText, baseURL, isMounted } = require('../utils');
 
 describe('SPA routes', () => {
   it('Call and inject onHttpRequest() data', async () => {
@@ -10,35 +10,27 @@ describe('SPA routes', () => {
 
   it('Inject error on onHttpRequest action', async () => {
     await page.goto(`${baseURL}/spa/on-http-request-error`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await checkText('h1', 'Error 500');
   });
 
   it('Display asyncData() data correctly', async () => {
     await page.goto(`${baseURL}/spa/async-data`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await isMounted();
     await checkText('#value', 'asyncData');
   });
 
   it('Call asyncData() and inject to store', async () => {
     await page.goto(`${baseURL}/spa/async-data-store`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await isMounted();
     await checkText('#value', 'asyncDataStore');
   });
 
   it('Error in asyncData() inject data to errorHandler', async () => {
     await page.goto(`${baseURL}/spa/async-data-error`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await checkText('h1', 'Error 500');
   });
 
@@ -51,25 +43,20 @@ describe('SPA routes', () => {
 
   it('404 error inject data to store', async () => {
     await page.goto(`${baseURL}/spa/not-found`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await checkText('h1', 'Error 404');
   });
 
   it('Call global middlewares', async () => {
     await page.goto(`${baseURL}/spa/global-middleware`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await isMounted();
     await checkText('#value', 'globalMiddleware');
   });
 
   it('Call route middlewares', async () => {
     await page.goto(`${baseURL}/spa/route-middleware`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
+    await isSPA();
     await isMounted();
     await checkText('#value', 'routeMiddleware');
   });
@@ -83,25 +70,19 @@ describe('SPA routes', () => {
 
   it('Middleware error', async () => {
     await page.goto(`${baseURL}/spa/middleware-error`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await checkText('h1', 'Error 500');
   });
 
   it('Middleware error with helper', async () => {
     await page.goto(`${baseURL}/spa/middleware-error-func`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await checkText('h1', 'Error 403');
   });
 
   it('Nested routes asyncData()', async () => {
     await page.goto(`${baseURL}/spa/nested`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await isMounted();
     await checkText('#parent-value', 'parent');
     await checkText('#value', 'child');
@@ -114,9 +95,7 @@ describe('SPA routes', () => {
 
   it('Call plugin init', async () => {
     await page.goto(`${baseURL}/spa/plugin`);
-    const { spa } = await readSSRData();
-    expect(spa).toBe(true);
-
+    await isSPA();
     await isMounted();
     await checkText('#value', 'plugin');
   });
